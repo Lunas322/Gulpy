31306ab99958ce8f2e0ff63bc97dcc66915d9378
heoseangjin <fpvmxm333477@gmail.com>
2026-06-25 17:38:01 +0900

    feat: Google 로그인 및 로그아웃 기능 구현

--- src/app/firebase/auth.ts
+++ src/app/firebase/auth.ts
@@ -1,3 +1,37 @@
-import { getAuth } from "firebase/auth";
+import {
+  getAuth,
+  GoogleAuthProvider,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
 import { app } from "./firebase";
+import { doc, setDoc } from "firebase/firestore";
+import { db } from "./firestore";
 export const auth = getAuth(app);
+
+export const provider = new GoogleAuthProvider();
+
+export const handleGoogleLogin = async () => {
+  try {
+    const result = await signInWithPopup(auth, provider);
+    const userData = result.user;
+
+    await setDoc(doc(db, "users", userData.uid), {
+      uid: userData.uid,
+      name: userData.displayName,
+      email: userData.email,
+      createdAt: new Date(),
+    });
+    return userData;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const handleGoogleLogout = async () => {
+  try {
+    signOut(auth);
+  } catch (erro) {
+    console.log(erro);
+  }
+};
