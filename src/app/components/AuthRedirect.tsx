a5c72a389e9864f3325d090d6a2b6a3e5becc34c
heoseangjin <fpvmxm333477@gmail.com>
2026-06-25 18:11:29 +0900

    fix: 인증 상태 로딩 중 리다이렉트 방지 추가

--- src/app/components/AuthRedirect.tsx
+++ src/app/components/AuthRedirect.tsx
@@ -3,6 +3,7 @@
 import React, { useEffect } from "react";
 import { useAuthStore } from "../store/authStore";
 import { usePathname, useRouter } from "next/navigation";
+import Loading from "./Loading";
 
 export default function AuthRedirect({
   children,
@@ -10,18 +11,22 @@ export default function AuthRedirect({
   children: React.ReactNode;
 }) {
   const user = useAuthStore((state) => state.user);
+  const loading = useAuthStore((state)=>state.loading)
   const router = useRouter();
   const pathname = usePathname();
   const isLoginPage = pathname.startsWith("/login");
 
   useEffect(() => {
+    if (loading) return
     if (user && isLoginPage) {
       router.replace("/");
     }
     if (!user && !isLoginPage) {
       router.replace("/login");
     }
-  }, [user, pathname]);
+
+  }, [user, pathname, loading]);
+
 
   return <>{children}</>;
 }
