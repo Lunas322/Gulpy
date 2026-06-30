32720a1eb381bda92ee0ab490ab4f50d34a11032
real man <fpvmxm333477@gmail.com>
2026-06-30 13:32:45 +0000

    feat: DrinkButtons 컴포넌트 추가

---
+++ src/app/components/DrinkButtons.tsx
@@ -0,0 +1,22 @@
+import useDrinkWater from "../hooks/useDrinkWater"
+
+
+export default function DrinkButtons() {
+    const waters = [{ water: 100, imoji: '🥛' },{ water: 200, imoji: '🥤' }, { water: 300, imoji: '💧' }, { water: 500, imoji: '🧴' }]
+    const { handleDrink } = useDrinkWater()
+    return (
+        <div className="grid grid-cols-2 gap-4">
+            {waters.map((data) => {
+                return (
+                    <button className="rounded-2xl bg-white py-5 shadow transition active:scale-95 active:bg-sky-100 active:shadow-inner"
+                        onClick={() => handleDrink(data.water)}
+                        key={data.water}
+                    >
+                        <p className="text-3xl">{data.imoji}</p>
+                        <p className="mt-2 font-semibold">{data.water}ml</p>
+                    </button>
+                )
+            })}
+        </div>
+    )
+}
