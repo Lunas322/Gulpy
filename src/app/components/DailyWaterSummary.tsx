bf138778dac9ae1772f1fee537e2a373b51ecaa9
real man <fpvmxm333477@gmail.com>
2026-06-30 13:32:30 +0000

    feat: DailyWaterSummary 컴포넌트 추가

---
+++ src/app/components/DailyWaterSummary.tsx
@@ -0,0 +1,33 @@
+import { Water } from "../types/water";
+import { getRemainingWater, getWaterPercent } from "../utils/calculateWaterIntakeDetails";
+
+
+type Props = {
+    data : Water
+}
+export default function DailyWaterSummary ({data}:Props) {
+    return(
+                <section className="mt-8 rounded-3xl bg-white p-6 shadow">
+                  <h3 className="mb-4 text-lg font-semibold">
+                    오늘 기록
+                  </h3>
+        
+                  <div className="space-y-3">
+                    <div className="flex justify-between">
+                      <span>목표 달성률</span>
+                      <span className="font-semibold">{getWaterPercent(data)} %</span>
+                    </div>
+        
+                    <div className="flex justify-between">
+                      <span>남은 양</span>
+                      <span className="font-semibold">{getRemainingWater(data as Water)}</span>
+                    </div>
+        
+                    <div className="flex justify-between">
+                      <span>마신 횟수</span>
+                      <span className="font-semibold">{data?.count}회</span>
+                    </div>
+                  </div>
+                </section>
+    )
+}
