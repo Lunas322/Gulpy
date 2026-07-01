24c460b8fb9191f88e9c3ec0e5719970dbe40251
real man <fpvmxm333477@gmail.com>
2026-07-01 08:28:31 +0000

    refactor: water type 오타 수정 onboarding

--- src/app/types/water.ts
+++ src/app/types/water.ts
@@ -1,9 +1,8 @@
-import { Timestamp } from "firebase/firestore"
 
 export type Water = {
     target: number
     interval: number
-    onboardin: boolean
+    onboarding: boolean
     uid: string
     count: number
     now: number
