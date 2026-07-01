b1475573deb968931a0cc676778c75346545c22e
real man <fpvmxm333477@gmail.com>
2026-07-01 06:07:32 +0000

    refactor: as 타입 삭제

--- src/app/components/DailyWaterSummary.tsx
+++ src/app/components/DailyWaterSummary.tsx
@@ -3,31 +3,31 @@ import { getRemainingWater, getWaterPercent } from "../utils/calculateWaterIntak
 
 
 type Props = {
-    data : Water
+  data: Water
 }
-export default function DailyWaterSummary ({data}:Props) {
-    return(
-                <section className="mt-8 rounded-3xl bg-white p-6 shadow">
-                  <h3 className="mb-4 text-lg font-semibold">
-                    오늘 기록
-                  </h3>
-        
-                  <div className="space-y-3">
-                    <div className="flex justify-between">
-                      <span>목표 달성률</span>
-                      <span className="font-semibold">{getWaterPercent(data)} %</span>
-                    </div>
-        
-                    <div className="flex justify-between">
-                      <span>남은 양</span>
-                      <span className="font-semibold">{getRemainingWater(data as Water)}</span>
-                    </div>
-        
-                    <div className="flex justify-between">
-                      <span>마신 횟수</span>
-                      <span className="font-semibold">{data?.count}회</span>
-                    </div>
-                  </div>
-                </section>
-    )
+export default function DailyWaterSummary({ data }: Props) {
+  return (
+    <section className="mt-8 rounded-3xl bg-white p-6 shadow  text-slate-500">
+      <h3 className="mb-4 text-lg font-semibold">
+        오늘 기록
+      </h3>
+
+      <div className="space-y-3">
+        <div className="flex justify-between">
+          <span>목표 달성률</span>
+          <span className="font-semibold">{getWaterPercent(data)} %</span>
+        </div>
+
+        <div className="flex justify-between">
+          <span>남은 양</span>
+          <span className="font-semibold">{getRemainingWater(data)}</span>
+        </div>
+
+        <div className="flex justify-between">
+          <span>마신 횟수</span>
+          <span className="font-semibold">{data?.count}회</span>
+        </div>
+      </div>
+    </section>
+  )
 }
