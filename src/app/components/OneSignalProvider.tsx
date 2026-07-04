14e673554665fc5f9773fa958911b782e47c0aa7
real man <fpvmxm333477@gmail.com>
2026-07-04 08:24:05 +0000

    fix: OneSignal Provider 비동기 처리

--- src/app/components/OneSignalProvider.tsx
+++ src/app/components/OneSignalProvider.tsx
@@ -5,9 +5,13 @@ import OneSignal from "react-onesignal";
 
 export default function OneSignalProvider() {
   useEffect(() => {
-    OneSignal.init({
-      appId: "16a9ee10-2ede-4d21-98fc-72addaf51408",
-    });
+    async function init() {
+  
+    await OneSignal.init({
+        appId: "16a9ee10-2ede-4d21-98fc-72addaf51408",
+      });
+    }
+    init()
   }, []);
 
   return null;
