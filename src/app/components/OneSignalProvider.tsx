d3287d463bd238ebeb515766df205c1d68100864
real man <fpvmxm333477@gmail.com>
2026-07-04 08:14:15 +0000

    fix: api ID 변경

--- src/app/components/OneSignalProvider.tsx
+++ src/app/components/OneSignalProvider.tsx
@@ -6,7 +6,7 @@ import OneSignal from "react-onesignal";
 export default function OneSignalProvider() {
   useEffect(() => {
     OneSignal.init({
-      appId: "285c2047-db30-4f21-a52f-2df8a34ba522",
+      appId: "16a9ee10-2ede-4d21-98fc-72addaf51408",
     });
   }, []);
 
