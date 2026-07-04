08643d6ad19f262f51d10f3e2577d6195fcc9441
real man <fpvmxm333477@gmail.com>
2026-07-04 11:00:50 +0000

    feat:  user 특정 함수 추가

--- src/app/components/OneSignalProvider.tsx
+++ src/app/components/OneSignalProvider.tsx
@@ -1,15 +1,19 @@
 "use client";
 
+
 import { useEffect } from "react";
 import OneSignal from "react-onesignal";
+import { useAuthStore } from "../store/authStore";
 
 export default function OneSignalProvider() {
+  const user = useAuthStore((state)=>state.user)
   useEffect(() => {
-    async function init() {
-  
-    await OneSignal.init({
-        appId: "16a9ee10-2ede-4d21-98fc-72addaf51408",
-      });
+    async function init() {  
+   await OneSignal.init({
+  appId: "16a9ee10-2ede-4d21-98fc-72addaf51408",
+});
+    if (!user?.uid) return
+await OneSignal.login(user.uid);
     }
     init()
   }, []);
