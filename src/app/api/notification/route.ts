4aa9a0ec398cd0b4a2e0cf707db419826acfdf64
real man <fpvmxm333477@gmail.com>
2026-07-05 13:13:02 +0000

    refactor: notification sendPush 재사용

--- src/app/api/notification/route.ts
+++ src/app/api/notification/route.ts
@@ -1,25 +1,9 @@
+import { sendPush } from "@/app/lib/onesignal";
 import { NextResponse } from "next/server";
 
 export async function POST(req: Request) {
     const { uid } = await req.json();
 
-    const response = await fetch("https://api.onesignal.com/notifications", {
-        method: "POST",
-        headers: {
-            Authorization: `Key ${process.env.ONESIGNAL_API_KEY}`,
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-            app_id: "16a9ee10-2ede-4d21-98fc-72addaf51408",
-            target_channel: "push",
-            include_aliases: {
-                external_id: [uid],
-            },
-            headings: { en: "💧 Gulpy" },
-            contents: { en: "물 마실 시간입니다!" },
-        }),
-    });
-
-    const data = await response.json();
-    return NextResponse.json(data);
+    const data = await sendPush(uid,0)
+    return NextResponse.json(data)
 }
